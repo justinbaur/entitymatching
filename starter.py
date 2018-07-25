73c0ce31b7fc4db8d2c8bf85a04a517ea7bfa5bc
justinbaur <justinbaur@users.noreply.github.com>
2018-07-25 01:40:37 -0500

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,8 @@
+import sys
+import py_entitymatching as em
+import pandas as pd
+import os
+
+print('python version: ' + sys.version)
+print('pandas version: ' + pd.__version__)
+print('magellan version: ' + em.__version__)
